f091a03ee84dad3e2db68d8dd3226e92ea28aa2a
Brufico <br.fischer@free.fr>
2018-01-18 23:51:18 +0100

    insteval2 cleaned

--- InstEval2/insteval2.R
+++ InstEval2/insteval2.R
@@ -1,7 +1,7 @@
 #' ---
 #' title: Exploration of the insteval dataset
 #' author "Bruno Fischer Colonimos"
-#' output: 
+#' output:
 #'     html_document:
 #'         number_section: yes
 #'         toc: yes
@@ -11,11 +11,11 @@
 
 
 #' ***********************************
-#' 
+#'
 #' ```{r setup, include=FALSE}
 #' knitr::opts_chunk$set(echo = TRUE)
 #' ```
-#' 
+#'
 
 suppressPackageStartupMessages(library(ggplot2))
 suppressPackageStartupMessages(library(dplyr))
@@ -47,25 +47,29 @@ sapply(df, function(col) {sum(is.na(col))} )
 
 
 # one-variable bar charts
-# studage
-ggplot(data = df, aes(x = studage)) + geom_bar( aes(y = ..prop..), width = 0.5)
-# lectage
-ggplot(data = df, aes(x = lectage)) + geom_bar( aes(y = ..prop..), width = 0.5)
-# service
-ggplot(data = df, aes(x = service)) + geom_bar( aes(y = ..prop..), width = 0.5)
-# dept
-ggplot(data = df, aes(x = dept)) + geom_bar( aes(y = ..prop..), width = 0.5)
-# y
-ggplot(data = df, aes(y)) + geom_bar( aes(y = ..prop..), width = 0.5) + 
-  geom_vline(xintercept = mean(df$y)) + 
-  geom_text(data = data.frame(mean_y = mean(df$y)), 
-            aes(x = mean_y, y = .2, label = format(mean_y, digits = 3)), 
-            hjust = -.1)
+# function
+g_bar <- function(varstr, dfr = df) {
+        vname <- as.name(varstr)
+        ggplot(data = dfr, aes_(vname)) +
+                geom_bar( aes(y = ..prop..), width = 0.5)
+}
+# actual
+g_bar("studage")
+g_bar("service")
+g_bar("dept")
 
-# y boxplot
-ggplot(data = df, aes(x = 1, y = y)) + geom_boxplot( width = 0.5) + 
+g_bar("y") +
+        geom_vline(xintercept = mean(df$y)) +
+        geom_text(data = data.frame(mean_y = mean(df$y)),
+                  aes(x = mean_y, y = .2, label = format(mean_y, digits = 3)),
+                  hjust = -.2)
+
+
+# y with boxplot
+
+ggplot(data = df, aes(x = 1, y = y)) + geom_boxplot( width = 0.5) +
   geom_point(data = data.frame(mean_y = mean(df$y)), aes(y = mean_y), size = 4, color = "red", alpha = .5 ) +
-  geom_text(data = data.frame(mean_y = mean(df$y)), aes(y = mean_y, label = format(mean_y, digits = 3)), 
+  geom_text(data = data.frame(mean_y = mean(df$y)), aes(y = mean_y, label = format(mean_y, digits = 3)),
             hjust = -.2)
 
 
@@ -79,42 +83,18 @@ myfacet <- function(row = ".", col = "."){
   facet_grid(fmla)
 }
 
-# -------------------------------------------------------------------------------
-
-rovar <- as.name("studage")
-covar <- as.name("lectage")
-mean_y_df <- df %>%
-  group_by_(rovar, covar) %>%
-  summarise(mean_y = mean(y))
-
-ybar_by <- function(vrow = "." , vcol = "." , labheight = .2) {
-  # compute the means by groups
-  rowvar <- as.name(vrow)
-  mean_y_df <- df %>%
-    group_by_(rowvar) %>%
-    summarise(mean_y = mean(y))
-  
-  # make the ggplot
-  ggplot(data = df, aes(y)) + geom_bar( aes(y = ..prop..), width = 0.5) +
-    geom_vline(data = mean_y_df, aes(xintercept = mean_y)) +
-    geom_text(data = mean_y_df, aes(x = mean_y, y = labheight, label = format(mean_y, digits = 3)), 
-              hjust = -.1) +
-    myfacet(vrow, vcol)
-}
-
-# -----------------------------------------------------------------------------------------
 
 ybar_by2 <- function(vrow = "." , vcol = "." , labheight = .2) {
   # print(missing(vcol))
   # compute the means by groups
-  
+
   mvrow <- missing(vrow)
   mvcol <- missing(vcol)
-  
+
   # function for grouping dataframes
   groupif <- function(dframe, vrow, vcol, mvrow, mvcol) {
        rowvar <- as.name(vrow)
-      colvar <- as.name(vcol)   
+      colvar <- as.name(vcol)
     if (!mvrow & ! mvcol) {
       group_by_(dframe, rowvar, colvar)
     } else if (!mvrow){
@@ -125,16 +105,16 @@ ybar_by2 <- function(vrow = "." , vcol = "." , labheight = .2) {
       dframe
     }
   }
-  
+
   # compute dataframe of conditional means
   mean_y_df <- df %>%
     groupif(vrow, vcol, mvrow, mvcol ) %>%
     summarise(mean_y = mean(y))
-  
+
   # make the ggplot
   ggplot(data = df, aes(y)) + geom_bar( aes(y = ..prop..), width = 0.5) +
     geom_vline(data = mean_y_df, aes(xintercept = mean_y)) +
-    geom_text(data = mean_y_df, aes(x = mean_y, y = labheight, label = format(mean_y, digits = 3)), 
+    geom_text(data = mean_y_df, aes(x = mean_y, y = labheight, label = format(mean_y, digits = 3)),
               hjust = -.1) +
     myfacet(vrow, vcol)
 }
@@ -144,13 +124,13 @@ ybar_by2 <- function(vrow = "." , vcol = "." , labheight = .2) {
 
 
 
-ybar_by("studage")
-ybar_by("lectage")
-ybar_by("service")
-ybar_by("dept")
+ybar_by2("studage")
+ybar_by2("lectage")
+ybar_by2("service")
+ybar_by2("dept")
 
 
-ybar_by2("studage", "lectage")
+ybar_by2("lectage", "studage")
 
 ybar_by2("studage", "service")
 ybar_by2("studage")
