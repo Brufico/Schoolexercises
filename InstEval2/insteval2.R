2f71331e6d455974906689fae7d70045049f4a6b
Brufico <br.fischer@free.fr>
2018-01-20 01:29:41 +0100

    before pulling

--- InstEval2/insteval2.R
+++ InstEval2/insteval2.R
@@ -61,15 +61,17 @@ g_bar("dept")
 g_bar("y") +
         geom_vline(xintercept = mean(df$y)) +
         geom_text(data = data.frame(mean_y = mean(df$y)),
-                  aes(x = mean_y, y = .2, label = format(mean_y, digits = 3)),
-                  hjust = -.2)
+                  aes(x = mean_y, label = format(mean_y, digits = 3)),
+                  y = .2 , hjust = -.2)
 
 
 # y with boxplot
 
 ggplot(data = df, aes(x = 1, y = y)) + geom_boxplot( width = 0.5) +
-  geom_point(data = data.frame(mean_y = mean(df$y)), aes(y = mean_y), size = 4, color = "red", alpha = .5 ) +
-  geom_text(data = data.frame(mean_y = mean(df$y)), aes(y = mean_y, label = format(mean_y, digits = 3)),
+  geom_point(data = data.frame(mean_y = mean(df$y)),
+             aes(y = mean_y), size = 4, color = "red", alpha = .5 ) +
+  geom_text(data = data.frame(mean_y = mean(df$y)),
+            aes(y = mean_y, label = format(mean_y, digits = 3)),
             hjust = -.2)
 
 
@@ -79,50 +81,50 @@ ggplot(data = df, aes(x = 1, y = y)) + geom_boxplot( width = 0.5) +
 
 
 myfacet <- function(row = ".", col = "."){
-  fmla <- as.formula(paste0(row, " ~ ", col))
-  facet_grid(fmla)
+        fmla <- as.formula(paste0(row, " ~ ", col))
+        facet_grid(fmla)
 }
 
 
 ybar_by2 <- function(vrow = "." , vcol = "." , labheight = .2) {
-  # print(missing(vcol))
-  # compute the means by groups
 
-  mvrow <- missing(vrow)
-  mvcol <- missing(vcol)
+        # compute the means by groups
+        # check missing args
+        mvrow <- missing(vrow)
+        mvcol <- missing(vcol)
 
-  # function for grouping dataframes
-  groupif <- function(dframe, vrow, vcol, mvrow, mvcol) {
-       rowvar <- as.name(vrow)
-      colvar <- as.name(vcol)
-    if (!mvrow & ! mvcol) {
-      group_by_(dframe, rowvar, colvar)
-    } else if (!mvrow){
-      group_by_(dframe, rowvar)
-    }else if (!mvcol) {
-      group_by_(dframe, colvar)
-    } else {
-      dframe
-    }
-  }
+        # function for grouping dataframes
+        groupif <- function(dframe, vrow, vcol, mvrow, mvcol) {
+                rowvar <- as.name(vrow)
+                colvar <- as.name(vcol)
+                if (!mvrow & !mvcol) {
+                        group_by_(dframe, rowvar, colvar)
+                } else if (!mvrow){
+                        group_by_(dframe, rowvar)
+                }else if (!mvcol) {
+                        group_by_(dframe, colvar)
+                } else {
+                        dframe
+                }
+        }
 
-  # compute dataframe of conditional means
-  mean_y_df <- df %>%
-    groupif(vrow, vcol, mvrow, mvcol ) %>%
-    summarise(mean_y = mean(y))
+        # compute dataframe of conditional means
+        mean_y_df <- df %>%
+                groupif(vrow, vcol, mvrow, mvcol ) %>%
+                summarise(mean_y = mean(y))
 
-  # make the ggplot
-  ggplot(data = df, aes(y)) + geom_bar( aes(y = ..prop..), width = 0.5) +
-    geom_vline(data = mean_y_df, aes(xintercept = mean_y)) +
-    geom_text(data = mean_y_df, aes(x = mean_y, y = labheight, label = format(mean_y, digits = 3)),
-              hjust = -.1) +
-    myfacet(vrow, vcol)
+        # make the ggplot
+        ggplot(data = df, aes(y)) +
+                geom_bar( aes(y = ..prop..), width = 0.5) +
+                geom_vline(data = mean_y_df, aes(xintercept = mean_y)) +
+                geom_text(data = mean_y_df,
+                          aes(x = mean_y,
+                              label = format(mean_y, digits = 3)),
+                          y = labheight, hjust = -.1) +
+                myfacet(vrow, vcol)
 }
 
-# -----------------------------------------------------------------------------------------
-
-
-
+# use that function
 
 ybar_by2("studage")
 ybar_by2("lectage")
@@ -134,15 +136,22 @@ ybar_by2("lectage", "studage")
 
 ybar_by2("studage", "service")
 ybar_by2("studage")
-ybar_by2(, "service")
+# ybar_by2(, "service")
 ybar_by2("service")
 ybar_by2("dept", "service")
 
 
-#  ========================================================================================================
+#  ===============================================================
 # comparing boxplots
-
-
+# =======------------===========------------=============---------
+ybox_by <- function(vx, vrow = ".", vcol = "." ) #########"" to do
+ggplot(data = df, aes_(x = 1, y = y)) +
+        geom_boxplot(width = 0.5) +
+        geom_point(data = data.frame(mean_y = mean(df$y)),
+                   aes(y = mean_y), size = 4, color = "red", alpha = .5 ) +
+        geom_text(data = data.frame(mean_y = mean(df$y)),
+                  aes(y = mean_y, label = format(mean_y, digits = 3)),
+                  hjust = -.2)
 
 
 
